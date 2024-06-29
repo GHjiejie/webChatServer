13035f5859bf4304ba6152210974a2703f03ab20
GHjiejie <3426571530@qq.com>
2024-06-29 18:12:44 +0800

    feat:用户身份验证中间件bug处理

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,32 +7,46 @@ const authMiddleware = (req, res, next) => {
   } else {
     // console.log("输出当前用户的请求路径,需要token", req.url);
     const token = req.headers.authorization;
-    // console.log("输出token", token);
-    if (token) {
-      next();
-      // // 下面的代码有一点问题
-      // const veriftResult = JWT.verifyToken(token, process.env.JWT_SECRET);
-      // if (veriftResult) {
-      //   // 获取当前的时间戳,用于更新用户的token
-      //   const currentTime = Date.now();
-      //   const newToken = JWT.generateToken(
-      //     {
-      //       username: result.username,
-      //       currentTime,
-      //     },
-      //     process.env.JWT_SECRET
-      //   );
-      //   res.header("Authorization", newToken);
-      //   next();
-      // } else {
-      //   res.send(401).json("token验证失败");
-      // }
-    } else {
-      res.json({
+    if (!token) {
+      return res.json({
         code: 401,
         message: "用户登录过期,请重新登录",
       });
     }
+    try {
+      const result = JWT.verifyToken(token, process.env.JWT_SECRET);
+      if (result) {
+        res.header("Authorization", token);
+        next();
+      } else {
+        return res.json({
+          code: 401,
+          message: "token验证失败，请重新登录",
+        });
+      }
+    } catch (error) {
+      return res.json({
+        code: 500,
+        message: "服务器内部错误",
+      });
+    }
+    // console.log("输出服务端生成的截取的token", token);
+
+    // if (token !== null) {
+    //   const result = JWT.verifyToken(token, process.env.JWT_SECRET);
+    //   // console.log("输出token验证的结果", result);
+    //   if (result) {
+    //     res.header("Authorization", token);
+    //     next();
+    //   } else {
+    //     console.log("token验证失败");
+    //   }
+    // } else {
+    //   res.json({
+    //     code: 401,
+    //     message: "用户登录过期,请重新登录",
+    //   });
+    // }
   }
 };
 module.exports = authMiddleware;
